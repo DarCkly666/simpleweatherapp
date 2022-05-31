baa6a252f494f56ba5e09e88573e724ede10c6a2
DarCkly666 <lessombremonsieur@--->
2022-05-30 21:09:18 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -9,8 +9,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <span class="rTNyH RZQOk">Photo by <a href="https://unsplash.com/@alanrobertjones?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Alan Jones</a> on <a href="https://unsplash.com/s/photos/sky?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
-  </span> */
-}
